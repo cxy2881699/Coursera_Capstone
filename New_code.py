7cc5a418f7e877691c61a728fd5411e47295146b
cxy2881699 <49932272+cxy2881699@users.noreply.github.com>
2019-08-01 04:15:39 -0500

    new test

---
+++ New_code.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# This notebook is mainly used for coursera capstone projects
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
